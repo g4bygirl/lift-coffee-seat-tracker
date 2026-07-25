2bd0b9d26bd7bc30a68c5b62190b65dab49a7c71
g4bygirl <gabriellenalejandro@gmail.com>
2026-07-24 23:01:25 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   vite: {},
   nitro: {
-    preset: "static",
-  },
-  tanstackStart: {
-    server: { entry: "server" },
+    preset: "vercel",
   },
 });
