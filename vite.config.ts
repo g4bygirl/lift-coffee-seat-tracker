1fbb4334d119c046a5b124692221f5d9ac321e71
g4bygirl <gabriellenalejandro@gmail.com>
2026-07-24 21:52:45 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,12 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   vite: {
-    // Sets the base path for assets to point to your GitHub repo
-    base: "/llift-coffee-seat-tracker/", // repo name
+    base: "/lift-coffee-seat-tracker/",
   },
-  
-  // Set Nitro output target to static SSG for GitHub Pages compatibility
   nitro: {
     preset: "static",
   },
-
   tanstackStart: {
-    // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
-    // nitro/vite builds from this
     server: { entry: "server" },
   },
 });
