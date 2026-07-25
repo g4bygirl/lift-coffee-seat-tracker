cb3540388667bf689c7c14282932abf0b7c76fbb
g4bygirl <gabriellenalejandro@gmail.com>
2026-07-24 22:26:07 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  vite: {
-    base: "/lift-coffee-seat-tracker/",
-  },
+  vite: {},
   nitro: {
     preset: "static",
   },
